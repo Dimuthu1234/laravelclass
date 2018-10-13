11adb42350152ea93e3d6d7ce4a59738fe1e73a1
hiruni1234 <samanphoto@gmail.com>
2018-10-13 11:09:31 +0530

    indexpage

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,64 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Blog Page</div>
+
+                <div class="card-body">
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <div class="container">
 	<div class="row">
 	<div class="col-md-12">
-<h1>My Blog Page Index File</h1>
+<h1><center>Blog Page</center></h1>
 <!-- Just an image -->
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-    <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
-  </a>
-</nav>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style>
-    /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
-    .row.content {height: 1500px}
-    
-    /* Set gray background color and 100% height */
-    .sidenav {
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-    
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-    
-    /* On small screens, set height to 'auto' for sidenav and grid */
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height: auto;} 
-    }
-  </style>
-</head>
-<body>
+
+ 
 
 <div class="container-fluid">
   <div class="row content">
     <div class="col-sm-3 sidenav">
-      <h4>John's Blog</h4>
-      <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="#section1">Home</a></li>
-        <li><a href="#section2">Friends</a></li>
-        <li><a href="#section3">Family</a></li>
-        <li><a href="#section3">Photos</a></li>
-      </ul><br>
       <div class="input-group">
         <input type="text" class="form-control" placeholder="Search Blog..">
         <span class="input-group-btn">
@@ -73,15 +58,15 @@
       <h4><small>RECENT POSTS</small></h4>
       <hr>
       <h2>My First Blog Post</h2>
-      <h5><span class="glyphicon glyphicon-time"></span> Post by Jane Dane, Sep 27, 2015.</h5>
+      <h5><span class="glyphicon glyphicon-time"></span> Post by chamodya, Sep 27, 2018.</h5>
       <h5><span class="label label-danger">Myself</span> <span class="label label-primary">Education</span></h5><br>
-      <p>Hi I am chamodya hirunika.I'm from Sri Lanka .I'm a undergraduate studdent of National School Of Business Management.I'm Learning Software Engineering For My degree.</p>
+      <p>Hi I am chamodya hirunika.I'm from Sri Lanka .I'm a undergraduate student of National School Of Business Management.I'm Learning Software Engineering For My degree.</p>
       <br><br>
       
-      <h4><small>RECENT POSTS</small></h4>
+      <h4><small>POSTS</small></h4>
       <hr>
       <h2>Officially Blogging</h2>
-      <h5><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
+      <h5><span class="glyphicon glyphicon-time"></span> Post by chamodya, Sep 24, 2018.</h5>
       <h5><span class="label label-success">Lorem</span></h5><br>
       <p>Sri Lanka (formerly Ceylon) is an island nation south of India in the Indian Ocean. Its diverse landscapes range from rainforest and arid plains to highlands and sandy beaches. It’s famed for its ancient Buddhist ruins, including the 5th-century citadel Sigiriya, with its palace and frescoes. The city of Anuradhapura, Sri Lanka's ancient capital, has many ruins dating back more than 2,000 years.</p>
       <hr>
@@ -130,12 +115,7 @@
   </div>
 </div>
 
-<footer class="container-fluid">
-  <p>Footer Text</p>
-</footer>
 
-</body>
-</html>
 
 
 	<div class="card" style="width: 18rem;">
